da35749543dd75d0df1d86223786ac59e1ba42f1
hbelhumeur <hbelhumeur@summit-tech.ca>
2018-03-01 15:02:40 -0500

    Removed all files that are in the .gitignore
    Change-Id: Ic37d404be0daf1322215186a5ea1540d1a292b5f

---
+++ unity-midi-master/unity-midi-master/Assets/Script/HugoScript/MidiToHumanInterpretor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MidiToHumanInterpretor : MonoBehaviour
+{
+
+	[SerializeField]
+	public string midiPath;
+
+	private string header;
+
+	private int headerLenght;
+
+	private int formatType;
+
+	private int numberOfTrack;
+
+	private int division;
+
+
+	private void Awake()
+	{
+		Debug.Log("MidiToHumanInterpretor: Awake()");
+		ReadMidiFile();
+	}
+
+
+	private void ReadMidiFile()
+	{
+		Debug.Log("ReadMidiFile()");
+		// Example Header:			4D 54 68 64 00 00 00 06 ff ff nn nn dd dd
+		//	separeted like this:	[4D 54 68 64] [00 00 00 06] [ff ff] [nn nn] [dd dd]
+		// Header format:  header_chunk = "MThd" + <header_length> + <format> + <n> + <division>
+
+		FileStream fs = new FileStream(midiPath, FileMode.Open);
+		int hexIn;
+		string hex = null;
+
+		for (int i = 0; (hexIn = fs.ReadByte()) != -1; i++)
+		{
+			hex += string.Format("{0:X2}", hexIn);
+			
+		}
+		if (!string.IsNullOrEmpty(hex))
+		{
+			Debug.Log(hex);
+		}
+
+	}
+
+}
