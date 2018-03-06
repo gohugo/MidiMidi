cbfd9877f627a9553c74c64b44ec008c7e691796
hbelhumeur <hbelhumeur@summit-tech.ca>
2018-03-06 13:19:42 -0500

    Creating MidiToCSV class
    will make me able to create csv version of the MidiFile that i will use
    later with TensorFlow.
    
    Change-Id: I1da990a5a647c1230b4a6189793f796679f95c6b

---
+++ unity-midi-master/unity-midi-master/Assets/Script/HugoScript/MidiFileToCSV.cs
@@ -0,0 +1,62 @@
+﻿using AudioSynthesis.Midi;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityMidi;
+
+public class MidiFileToCSV : MonoBehaviour
+{
+
+
+
+	[SerializeField]
+	StreamingAssetResouce midiSource;
+
+	public Author author;
+
+	private MidiFile midiFile;
+
+
+
+	public enum Author
+	{
+		Bach, Beth, Choppin, Other
+	}
+
+
+
+	void Start()
+	{
+		midiFile = new MidiFile(midiSource);
+		CreateCSVFile();
+
+
+	}
+
+	private void CreateCSVFile()
+	{
+		MidiTrack[] tracks = midiFile.Tracks;
+
+		string midiName = Path.GetFileNameWithoutExtension(midiSource.GetName());
+
+		int ntrack = tracks.Length;
+		//before your loop
+		var csv = new StringBuilder();
+
+
+		//in your loop
+
+		//Suggestion made by KyleMit
+		var newLine = string.Format("{0},{1}", midiName, ntrack);
+		csv.AppendLine(newLine);
+
+		string path = Path.Combine(Path.Combine(Application.streamingAssetsPath, "Created"), midiName + ".csv");
+		Debug.Log(path);
+		//after your loop
+		File.WriteAllText(path, csv.ToString());
+
+	}
+
+}
