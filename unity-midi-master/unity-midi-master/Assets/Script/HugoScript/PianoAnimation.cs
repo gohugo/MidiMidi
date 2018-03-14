7751a9d709af06c54197b57fe8f66d92a67a61f8
hbelhumeur <hbelhumeur@summit-tech.ca>
2018-03-14 19:17:58 -0400

    Modified PianoPrefab Now the key are separeted
    Change-Id: I5eb76a3f8ec978c83636892ebb4123b6c82c0934

---
+++ unity-midi-master/unity-midi-master/Assets/Script/HugoScript/PianoAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoAnimation : MonoBehaviour {
+	public MeshRenderer[] meshRenderers;
+	public bool[] keysOn;
+
+
+	void Init() {
+		keysOn = new bool[meshRenderers.Length];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
